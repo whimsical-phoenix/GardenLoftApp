73d97e5dfd02ebcfc6fb7ef5e2f737092ddf1874
whimsical-phoenix <shahprapti12@gmail.com>
2024-03-04 11:52:22 -0700

    app init

---
+++ GardenLoftApp/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Started building Garden loft!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: "grey",
+    backgroundColor: "#f3b717",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
